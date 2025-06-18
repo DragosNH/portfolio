5e1c192df47fc6dfa416add619a145962df674e6
Dragos <edragos33@gmail.com>
2025-06-18 13:14:05 +0200

    cube added

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 // ------ Scene ------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 35);
+camera.position.set(0, 0, 10);
 
 // ------ Renderer ------
 const renderer = new THREE.WebGLRenderer();
@@ -21,8 +21,14 @@ window.addEventListener('resize', () => {
     renderer.setSize(width, height);
 });
 
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0xf0f0f0});
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
 
 
+// ------ Animations ------
 function animate(){
 
 
