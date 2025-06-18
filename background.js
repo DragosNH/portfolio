fe7fbdaa4d85fd63ec905eaeceb4e99c55231d14
Dragos <edragos33@gmail.com>
2025-06-18 14:16:07 +0200

    sphere added

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerH
 camera.position.set(0, 0, 35);
 
 // ------ Background ------
-scene.background = new THREE.CubeTextureLoader()
+scene.environment = new THREE.CubeTextureLoader()
     .setPath('textures/cubemap/')
     .load([
         'px.png',
@@ -16,14 +16,14 @@ scene.background = new THREE.CubeTextureLoader()
 		'ny.png',
 		'pz.png',
 		'nz.png'
-    ])
+    ]);
 
 // ------ Renderer ------
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls( camera, renderer.domElement )
+const controls = new OrbitControls( camera, renderer.domElement );
 
 // ------ Responsive Background ------
 window.addEventListener('resize', () => {
@@ -36,9 +36,16 @@ window.addEventListener('resize', () => {
     renderer.setSize(width, height);
 });
 
+// ------ Lighting ------
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+
 //------ Objects ------ 
+const mainGeo = new THREE.SphereGeometry(5,32,16);
+const mainMat = new THREE.MeshPhysicalMaterial();
+const mainSphere = new THREE.Mesh(mainGeo, mainMat);
 
-
+scene.add(mainSphere);
 
 // ------ Animations ------
 function animate(){
