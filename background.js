db46a774075dbbe64dda09b7b4e6c7e6c1b57211
Dragos <edragos33@gmail.com>
2025-06-18 13:09:39 +0200

    three background added

---
+++ background.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+// ------ Scene ------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 35);
+
+// ------ Renderer ------
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// ------ Responsive Background ------
+window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+});
+
+
+
+function animate(){
+
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+animate();
