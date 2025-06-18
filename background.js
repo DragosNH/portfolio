cdbb509c141d0acf756a9136912808fb33ca25d6
Dragos <edragos33@gmail.com>
2025-06-18 14:25:36 +0200

    sphere turned transparent

--- background.js
+++ background.js
@@ -37,12 +37,27 @@ window.addEventListener('resize', () => {
 });
 
 // ------ Lighting ------
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const directionalLight = new THREE.DirectionalLight( 0x00ffff, 1 );
 scene.add( directionalLight );
 
 //------ Objects ------ 
 const mainGeo = new THREE.SphereGeometry(5,32,16);
-const mainMat = new THREE.MeshPhysicalMaterial();
+const mainMat = new THREE.MeshPhysicalMaterial({
+    color: 0xffffff,
+	transmission: 1,
+	opacity: 1,
+	metalness: 0,
+	roughness: 0,
+	ior: 1.5,
+	thickness: 0.01,
+	specularIntensity: 1,
+	specularColor: 0xffffff,
+	envMapIntensity: 1,
+	lightIntensity: 1,
+	exposure: 1,
+	transmissionResolutionScale: 1
+});
+
 const mainSphere = new THREE.Mesh(mainGeo, mainMat);
 
 scene.add(mainSphere);
