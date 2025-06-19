e0ba21b488e0d0f82376f3967306f31833a7b82f
Dragos <edragos33@gmail.com>
2025-06-19 13:18:15 +0200

    penn added to the logo

--- logo.js
+++ logo.js
@@ -6,7 +6,7 @@ const logoContainer = document.querySelector('.logo');
 // ------ Scene ------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 50);
+camera.position.set(0, 0, 80);
 
 // ------ Renderer ------
 const renderer = new THREE.WebGLRenderer({ alpha: true});
@@ -32,7 +32,7 @@ const controls = new OrbitControls( camera, renderer.domElement );
 
 // ------ Objects ------
 // --- Background Circle ---
-const circleGeo = new THREE.CircleGeometry(30, 32);
+const circleGeo = new THREE.CircleGeometry(50, 32);
 const circleMat = new THREE.MeshBasicMaterial({
 	color: 0x13333e,
 	side: THREE.DoubleSide,
@@ -40,7 +40,7 @@ const circleMat = new THREE.MeshBasicMaterial({
 const circle = new THREE.Mesh(circleGeo, circleMat);
 
 // --- Pen body ---
-const cilinderGeo = new THREE.CylinderGeometry(3.5, 3.5, 35, 36);
+const cilinderGeo = new THREE.CylinderGeometry(5.5, 5.5, 65, 36);
 const cilinderMat = new THREE.MeshBasicMaterial({
 	color: 0xadd8e6
 });
@@ -49,52 +49,60 @@ penBody.position.y += 7.5;
 
 
 // --- Button ---
-const btnGeo = new THREE.BoxGeometry(3, 6, 6);
+const btnGeo = new THREE.BoxGeometry(4.5, 12, 7);
 const btnMat = new THREE.MeshBasicMaterial({
 	color: 0x13333e,
 });
 const penBtn = new THREE.Mesh(btnGeo, btnMat);
-penBtn.position.y += 12;
-penBtn.position.z += 1;
+penBtn.position.y += 20;
+penBtn.position.z += 3.5;
 
 
 // --- Torus around pen body ---
-const torusGeo = new THREE.TorusGeometry(4, 0.5, 15, 40);
+const torusGeo = new THREE.TorusGeometry(7, 1, 15, 40);
 const torusMat = new THREE.MeshBasicMaterial({
 	color: 0x13333e,
 });
 const torus = new THREE.Mesh(torusGeo, torusMat);
 torus.rotation.x = 4.7;
-torus.position.y -= 7.5;
+torus.position.y -= 19;
 
 
 // --- Pen head ---
-const penHeadGeo = new THREE.CylinderGeometry(3.5, 1.8, 12, 36);
+const penHeadGeo = new THREE.CylinderGeometry(5.5, 1.8, 15, 36);
 const penHeadMat = new THREE.MeshBasicMaterial({
 	color: 0xadd8e6,
 });
 const penHead = new THREE.Mesh(penHeadGeo, penHeadMat);
-penHead.position.y -= 17.5;
+penHead.position.y -= 35;
 
 // --- Pointer ---
 const pointerGeo = new THREE.CylinderGeometry(1.6, 0.5, 5, 36);
 const pointer = new THREE.Mesh(pointerGeo, penHeadMat);
-pointer.position.y -= 25;
+pointer.position.y -= 45;
 
 
 // ------ Groups ------
+// --- Body group ---
 const penBodyGroup = new THREE.Group();
 penBodyGroup.add(penBody);
 penBodyGroup.add(penBtn);
 penBodyGroup.add(torus);
-// penBodyGroup.rotation.z -= 0.5;
+// --- Head Group ---
+const headGroup = new THREE.Group();
+headGroup.add(penHead);
+headGroup.add(pointer);
+// --- Pen Group ---
+const penGroup = new THREE.Group();
+penGroup.add(penBodyGroup);
+penGroup.add(headGroup);
+
+penGroup.rotation.z -= 0.5;
 
 
 // ------ Objects added in the scene ------
 scene.add(circle);
-scene.add(penBodyGroup);
-scene.add(penHead); // for now
-scene.add(pointer); // for now
+scene.add(penGroup);
 
 function animate() {
 
